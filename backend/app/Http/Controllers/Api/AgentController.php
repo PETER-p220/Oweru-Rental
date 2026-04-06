1adc14632250a0c805f4ee21714e5db05612681b
PETER-p220 <mushyp420@gmail.com>
2026-04-06 16:44:21 +0300

    lead

--- backend/app/Http/Controllers/Api/AgentController.php
+++ backend/app/Http/Controllers/Api/AgentController.php
@@ -12,6 +12,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 
